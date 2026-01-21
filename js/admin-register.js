5896bb2ce42b3a3c9fb479b5b9b44b59aa45b811
rizaaamore-jpg <rizaaamore@gmail.com>
2026-01-21 09:00:46 +0700

    Create admin-register.js

---
+++ js/admin-register.js
@@ -0,0 +1,9 @@
+localStorage.setItem("admins", JSON.stringify([
+  {user:"admin", pass:"12345"},
+  {user:"admin2", pass:"67890"}
+]))
+
+localStorage.setItem("siswa", JSON.stringify([
+  {user:"arif", pass:"111"},
+  {user:"budi", pass:"222"}
+]))
