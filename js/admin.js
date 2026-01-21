a68d4369f6dedf255b8ac43a626eb00dc0e8fc76
rizaaamore-jpg <rizaaamore@gmail.com>
2026-01-21 09:04:04 +0700

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -6,3 +6,25 @@ function simpan(){
   localStorage.setItem("jadwal", jadwal.value);
   alert("Jadwal disimpan");
 }
+
+function tambahAdmin() {
+  let admins = JSON.parse(localStorage.getItem("admins")) || [];
+
+  let u = newAdmin.value.trim();
+  let p = newPass.value.trim();
+
+  if (!u || !p) {
+    alert("Lengkapi data");
+    return;
+  }
+
+  if (admins.find(a => a.user === u)) {
+    alert("Admin sudah ada");
+    return;
+  }
+
+  admins.push({ user: u, pass: p });
+  localStorage.setItem("admins", JSON.stringify(admins));
+
+  alert("Admin berhasil ditambahkan");
+}
