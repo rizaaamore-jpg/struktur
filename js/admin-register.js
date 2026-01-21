64ca5fbcea8253f7119115db560bad55cf826769
rizaaamore-jpg <rizaaamore@gmail.com>
2026-01-21 11:50:56 +0700

    Delete js/admin-register.js

--- js/admin-register.js
+++
@@ -1,9 +0,0 @@
-localStorage.setItem("admins", JSON.stringify([
-  {user:"admin", pass:"12345"},
-  {user:"admin2", pass:"67890"}
-]))
-
-localStorage.setItem("siswa", JSON.stringify([
-  {user:"arif", pass:"111"},
-  {user:"budi", pass:"222"}
-]))
